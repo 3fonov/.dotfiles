51d57af97ab93b60807c13e483afb18b4f445d05
Vladimir Trifonov <3fonov@gmail.com>
2024-11-24 11:53:22 +0300

    Update

--- nvim/.config/nvim/lua/user/keymaps.lua
+++ nvim/.config/nvim/lua/user/keymaps.lua
@@ -30,9 +30,14 @@ vim.keymap.set({ 'x', 'n' }, '<leader>p', '"_dP')
 
 -- DBT
 
-vim.keymap.set('n', '<leader>dd', ':! dbt run -s %:t:r<cr>')
-vim.keymap.set('n', '<leader>df', ':! dbt run -s %:t:r -f<cr>')
-vim.keymap.set('n', '<leader>dp', ':! dbt run -s %:t:r+<cr>')
+
+	vim.keymap.set('n', '<leader>dc', ':DbtCompile<CR>', { noremap = true, silent = true })
+	vim.keymap.set('n', '<leader>dy', ':DbtModelYaml<CR>', { noremap = true, silent = true })
+	vim.keymap.set('n', '<leader>dr', ':DbtRun<CR>', { noremap = true, silent = true })
+	vim.keymap.set('n', '<leader>df', ':DbtRunFull<CR>', { noremap = true, silent = true })
+	vim.keymap.set('n', '<leader>dt', ':DbtTest<CR>', { noremap = true, silent = true })
+	vim.keymap.set('n', '<leader>d+', ':DbtListUpstreamModels<CR>', { noremap = true, silent = true })
+	vim.keymap.set('n', '<leader>d-', ':DbtListDownstreamModels<CR>', { noremap = true, silent = true })
 
 -- Rest nvim
 
@@ -52,3 +57,20 @@ vim.keymap.set('n','<leader>gp',":Neogit pull<CR>",{silent=true, noremap=true})
 vim.keymap.set('n','<leader>gP',":Neogit push<CR>",{silent=true, noremap=true})
 vim.keymap.set('n','<leader>gb',":Telescope git_branches<CR>",{silent=true, noremap=true})
 vim.keymap.set('n','<leader>gB',":G blame<CR>",{silent=true, noremap=true})
+
+-- move
+--
+local opts = { noremap = true, silent = true }
+-- Normal-mode commands
+vim.keymap.set('n', '<A-j>', ':MoveLine(1)<CR>', opts)
+vim.keymap.set('n', '<A-k>', ':MoveLine(-1)<CR>', opts)
+vim.keymap.set('n', '<A-h>', ':MoveHChar(-1)<CR>', opts)
+vim.keymap.set('n', '<A-l>', ':MoveHChar(1)<CR>', opts)
+vim.keymap.set('n', '<leader>wf', ':MoveWord(1)<CR>', opts)
+vim.keymap.set('n', '<leader>wb', ':MoveWord(-1)<CR>', opts)
+
+-- Visual-mode commands
+vim.keymap.set('v', '<A-j>', ':MoveBlock(1)<CR>', opts)
+vim.keymap.set('v', '<A-k>', ':MoveBlock(-1)<CR>', opts)
+vim.keymap.set('v', '<A-h>', ':MoveHBlock(-1)<CR>', opts)
+vim.keymap.set('v', '<A-l>', ':MoveHBlock(1)<CR>', opts)
